06945d69d5733d69ea2cd4478bce939888adb3b4
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2024-02-22 17:39:02 +0100

    minor changes

--- block0/solution/exe2/python/tutor.py
+++ block0/solution/exe2/python/tutor.py
@@ -1,7 +1,6 @@
 # import packages
 
 import numpy as np
-from scipy.stats import boltzmann
 import matplotlib.pyplot as plt
 
 # calculate normal distribution
@@ -10,39 +9,33 @@ def normal_dist(x, mean, var):
     prob_density = np.exp(-0.5*((x-mean)/std)**2) / (std*(2*np.pi)**0.5)
     return prob_density
 
-# main function
-def main():
-    # load data
-    data = np.loadtxt("exercise_2.dat")
-    
-    # generate normal distribution data
-    x = np.arange(-5, 5, 0.1)
-    
-    m1, var1 = 0, 0.2
-    m2, var2 = 0, 1.0
-    m3, var3 = -1, 0.2
 
-    y1 = normal_dist(x, m1, var1)
-    y2 = normal_dist(x, m2, var2)
-    y3 = normal_dist(x, m3, var3)
-    
-    # make plots
-    fig, ax = plt.subplots(1, 2, figsize=(14,6))
+# load data
+data = np.loadtxt("exercise_2.dat")
 
-    ax[0].plot(data[:, 0], data[:, 1])
+# generate normal distribution data
+x = np.arange(-5, 5, 0.1)
 
-    ax[1].plot(x, y1, label=f"$\mu$=%s, $\sigma^{2}$=%s" %(m1, var1))
-    ax[1].plot(x, y2, label=f"$\mu$=%s, $\sigma^{2}$=%s" %(m2, var2))
-    ax[1].plot(x, y3, label=f"$\mu$=%s, $\sigma^{2}$=%s" %(m3, var3))
+m1, var1 = 0, 0.2
+m2, var2 = 0, 1.0
+m3, var3 = -1, 0.2
 
-    ax[1].set_xlabel("x")
-    ax[1].set_ylabel("$\phi_{\mu, \sigma^{2}}(x)$")
-    ax[1].set_ylim(-0.05, 1.05)
-    ax[1].legend()
+y1 = normal_dist(x, m1, var1)
+y2 = normal_dist(x, m2, var2)
+y3 = normal_dist(x, m3, var3)
 
-    fig.savefig("exe2.png")
+# make plots
+fig, ax = plt.subplots(1, 2, figsize=(14,6))
 
+ax[0].plot(data[:, 0], data[:, 1])
 
-# run the function
-if __name__ == '__main__':
-    main()
+ax[1].plot(x, y1, label=f"$\mu$=%s, $\sigma^{2}$=%s" %(m1, var1))
+ax[1].plot(x, y2, label=f"$\mu$=%s, $\sigma^{2}$=%s" %(m2, var2))
+ax[1].plot(x, y3, label=f"$\mu$=%s, $\sigma^{2}$=%s" %(m3, var3))
+
+ax[1].set_xlabel("x")
+ax[1].set_ylabel("$\phi_{\mu, \sigma^{2}}(x)$")
+ax[1].set_ylim(-0.05, 1.05)
+ax[1].legend()
+
+fig.savefig("exe2.png")
