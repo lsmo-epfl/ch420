d9fc1a3a284aa660a2a029036aae8f62dfda5a88
yutli <yutli@tsf-500-wpa-4-127.epfl.ch>
2024-02-26 16:55:41 +0100

    update on block0 bash part

---
+++ block0/solution/bash/exercise.sh
@@ -0,0 +1,12 @@
+cd /Users/yutli/Downloads #2
+cp Block\ 0\ -\ Computational\ Carpentry\ Solution-20240219 Block0 #2
+cp Block0 /Users/yutli/project/TA/2024spring/exercise #3
+rm -rf Block0 #4
+cd /Users/yutli/project/TA/2024spring/exercise #5
+mkdir Exercise_1 #6
+pwd #7
+cd Exercise_1 #8
+vim Exercise_1.sh #8
+chmod +x Exercise_1.sh #9
+./Exercise_1.sh #9
+
